337f5f5a24f2d83a01cce651ae457c265e624230
Caren Braj <carenbraj@gmail.com>
2020-07-21 14:15:55 -0700

    change entire code

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
-function generate(){
+const characterAmountRange = document.getElementById
+("characterAmountRange")
+const characterAmountNumber = document.getElementById
+("characterAmountNumber")
+const includeUppercaseElement = document.getElementById
+("includeUppercase")
+const includeNumbersElement = document.getElementById ("includeNumbers")
+const includeSymbolsElement = document.getElementById ("includeSymbols")
+const form = document.getElementById("passwordGeneratoForm")
+const passwordDisplay = document.getElementById("passwordDisplay")
 
-    //set password length and conplexity
-    let complexity = document.getElementById("slider").value;
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NNUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)
+).concat(arrayFromLowToHigh(91, 96) 
+).concat(arrayFromLowToHigh(123, 126)
+)
 
-    let values ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
+characterAmountRange.addEventListener("input", syncCharacterAmount)
 
-    let password = "";
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    const password = generatePassword(characterAmount, includeUppercase,includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+})
 
-    //create a for loop to choos passowrd characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1))); 
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if (includeUppercase) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    const passwordCharacters = []
+    for (let i = 0; i < characterAmount; i++) {
+        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode)) 
         
     }
+    return passwordCharacters.join("")
+}
 
-    //add password to textbox/display area
-    document.getElementById("display").value = password;
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = 0; i < array.length; i++) {
+        array.push(i)      
+    }
+    return array
+}
 
 
+function syncCharacterAmount(e) {
+    const value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value
 }
